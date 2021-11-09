70a8409322de65c2393ef01f8e29fcd74bd2b4cc
aristophanesyates <56827855+aristophanesyates@users.noreply.github.com>
2021-11-09 22:40:12 +0000

    Added some variables

--- Assets/Spin.cs
+++ Assets/Spin.cs
@@ -7,8 +7,11 @@ public class Spin : MonoBehaviour
     //float timer;
     public float speed;
     public float speedScale;
-    public Quaternion tilt;
-    public Quaternion angle;
+    public float Min;
+    public float Max;
+    public float TimeToStop;
+    private Quaternion tilt;
+    private Quaternion angle;
     // Start is called before the first frame update
     void Start()
     {
@@ -20,11 +23,11 @@ public class Spin : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        speed -= Time.deltaTime / TimeToStop;
         speed = Mathf.Clamp(speed, 0, 1);
         //timer += Time.deltaTime;
-        //speed += Mathf.Sin(timer);
 
-        tilt.eulerAngles = new Vector3(Mathf.Lerp(90, 2, speed), 0, 0);
+        tilt.eulerAngles = new Vector3(Mathf.Lerp(Max, Min, speed), 0, 0);
         angle.eulerAngles = angle.eulerAngles + new Vector3(0, speed * speedScale, 0);
         transform.rotation = angle * tilt;
     }
